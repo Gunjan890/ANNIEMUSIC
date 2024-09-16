1c818d36d158b5ebc2dc4e679eee4d21ef232991
Gunjan <165020412+Gunjan890@users.noreply.github.com>
2024-09-17 03:27:52 +0530

    Update call.py

--- ANNIEMUSIC/core/call.py
+++ ANNIEMUSIC/core/call.py
@@ -5,11 +5,11 @@ from typing import Union
 
 from pyrogram import Client
 from pyrogram.types import InlineKeyboardMarkup
-from ntgcalls import TelegramServerError
 from pytgcalls import PyTgCalls
 from pytgcalls.exceptions import (
     AlreadyJoinedError,
     NoActiveGroupCall,
+    TelegramServerError,
 )
 from pytgcalls.types import (
     MediaStream,
