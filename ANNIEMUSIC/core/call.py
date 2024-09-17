b5af53a14d27dca88eae80e84740f7d045144c98
Gunjan <165020412+Gunjan890@users.noreply.github.com>
2024-09-17 08:28:49 +0530

    Update call.py

--- ANNIEMUSIC/core/call.py
+++ ANNIEMUSIC/core/call.py
@@ -12,7 +12,6 @@ from pytgcalls.exceptions import (
     
 )
 from pytgcalls.types import Update
-from pytgcalls.types.input_stream import StreamAudioEnded
 from pytgcalls.types.input_stream.quality import HighQualityAudio, MediumQualityVideo
 from pytgcalls.types.stream import StreamAudioEnded
 
