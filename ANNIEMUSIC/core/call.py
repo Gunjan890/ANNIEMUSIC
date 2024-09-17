c9069b42889f61c00c8e9304ce41ab56793d43cf
Gunjan <165020412+Gunjan890@users.noreply.github.com>
2024-09-17 08:14:35 +0530

    Update call.py

--- ANNIEMUSIC/core/call.py
+++ ANNIEMUSIC/core/call.py
@@ -5,18 +5,17 @@ from typing import Union
 
 from pyrogram import Client
 from pyrogram.types import InlineKeyboardMarkup
-from pytgcalls import PyTgCalls
+from pytgcalls import PyTgCalls, StreamType
 from pytgcalls.exceptions import (
     AlreadyJoinedError,
     NoActiveGroupCall,
-    
+    TelegramServerError,
 )
 from pytgcalls.types import Update
 from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
 from pytgcalls.types.input_stream.quality import HighQualityAudio, MediumQualityVideo
 from pytgcalls.types.stream import StreamAudioEnded
 
-   
 import config
 from ANNIEMUSIC import LOGGER, YouTube, app
 from ANNIEMUSIC.misc import db
